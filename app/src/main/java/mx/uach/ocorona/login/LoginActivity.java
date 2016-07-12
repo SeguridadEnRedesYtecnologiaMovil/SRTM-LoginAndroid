15f793dea234b78908f88e9dc043dc1394223161
ocorona <a245329@uach.mx>
2016-07-11 20:26:26 -0600

    Se define el estilo material para la aplicacion

--- app/src/main/java/mx/uach/ocorona/login/LoginActivity.java
+++ app/src/main/java/mx/uach/ocorona/login/LoginActivity.java
@@ -1,9 +1,13 @@
 package mx.uach.ocorona.login;
 
+import android.app.Activity;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class LoginActivity extends AppCompatActivity {
+//public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
